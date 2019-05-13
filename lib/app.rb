3a970aaa0e9cc4a2eeb17f66ecdff1e2d50b3389
Student <comsin.c.croitoriu@gmail.com>
2019-05-13 17:05:21 +0100

    moved HTML into index.erb file

--- lib/app.rb
+++ lib/app.rb
@@ -5,17 +5,8 @@ get '/' do
     "Hello World"
   end
 
-#   get '/cat' do
-#     "<div>
-#     <img src='http://bit.ly/1eze8aE'>
-#     </div>"
-#   end
-
 get '/cat' do
-    "<div style='border: 3px dashed red'>
-      
-      <img src='http://bit.ly/1eze8aE'>
-     </div>"
+    erb(:index)
   end
   
 get '/doggo' do
