6099f42914344d93a80d6049159ec85a3b48b0be
Student <comsin.c.croitoriu@gmail.com>
2019-05-13 15:59:28 +0100

    use shotgun to handle sinatra server

---
+++ Gemfile
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# gem "rails"
+gem "sinatra-2.0.5"
+gem "shotgun-0.9.2"
---
+++ lib/app.rb
@@ -0,0 +1,14 @@
+require 'sinatra'
+
+
+get '/' do
+    "Hello World"
+  end
+
+  get '/cat' do
+    "Melted Cat's Site for other melted cats"
+  end
+
+get '/doggo' do
+  "arf arf"
+end
