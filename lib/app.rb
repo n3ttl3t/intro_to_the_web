299bf95a7a357589b7ace27456342733f72c96eb
Nicholas Barnes <n3ttl3t@gmail.com>
2019-05-14 18:35:40 +0100

    installed cabybara end selenium gems for automated browser testing

--- lib/app.rb
+++ lib/app.rb
@@ -6,15 +6,20 @@ get '/' do
 
 get '/random_cat' do
     @name = ["Cat", "HyperCat", "Melted Cat"].sample
-    erb(:index)
+    erb(:named_cat)
   end
 
-  get '/named_cat' do
-    p params
-    @name = params[:name]
-    erb(:index)
-  end
-  
+get '/cat_form' do
+  erb(:cat_form)
+end
+
+post '/named_cat' do
+  p params
+  @name = params[:name]
+  erb(:named_cat)
+end
+
+
 get '/doggo' do
   "arf arf"
 end
